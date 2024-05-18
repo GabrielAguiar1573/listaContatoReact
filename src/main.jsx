164c5d634cb2ddd897678e2b6603e011264e5054
GabrielAguiar1573 <gabrielhenriquedeaguiar@gmail.com>
2024-05-18 18:10:37 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ListaDeContatos from './listaContatos.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ListaDeContatos />
+  </React.StrictMode>,
+)
